b6278276c67c005f7c40efdcac171a93448d2457
bluefuton <chris@bluefuton.com>
2017-04-20 17:55:31 +0200

    Fix iffy merge

--- client/reader/following-manage/subscriptions.jsx
+++ client/reader/following-manage/subscriptions.jsx
@@ -1,27 +1,53 @@
 /**
  * External Dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { getReaderFollows } from 'state/selectors';
 
 /**
  * Internal Dependencies
  */
 import ReaderImportButton from 'blocks/reader-import-button';
 import ReaderExportButton from 'blocks/reader-export-button';
-import SortControls from './sort-controls';
+import SitesWindowScroller from './sites-window-scroller';
+import QueryReaderFollows from 'components/data/query-reader-follows';
+import FollowingManageSortControls from './sort-controls';
+import FollowingManageSearchFollowed from './search-followed';
 
 class FollowingManageSubscriptions extends Component {
+	static propTypes = {
+		follows: PropTypes.array.isRequired,
+	}
+
 	render() {
+		const { follows, width, translate } = this.props;
 		return (
 			<div className="following-manage__subscriptions">
+				<QueryReaderFollows />
 				<div className="following-manage__subscriptions-controls">
+					{
+						translate( '%(num)s Followed Sites', {
+							args: { num: follows.length }
+						} )
+					}
 					<ReaderImportButton />
 					<ReaderExportButton />
-					<SortControls />
+					<FollowingManageSortControls />
+					<FollowingManageSearchFollowed />
+				</div>
+				<div className="following-manage__subscriptions-list">
+					<SitesWindowScroller
+						sites={ follows }
+						width={ width }
+					/>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default FollowingManageSubscriptions;
+export default connect(
+	state => ( { follows: getReaderFollows( state ) } ),
+)( localize( FollowingManageSubscriptions ) );
