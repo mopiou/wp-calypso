8a5c20eff2c1e2de5c8699d325a7951d1f882573
Tugdual de Kerviler <dekervit@gmail.com>
2017-04-21 10:30:41 +0200

    Add 2FA components (unconnected)

---
+++ client/blocks/login/two-factor-authentication/index.jsx
@@ -0,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import VerificationCodeInput from './verification-code-input';
+import WaitingTwoFactorNotificationApproval from './waiting-notification-approval';
+import { localize } from 'i18n-calypso';
+
+class Login2FA extends Component {
+	static propTypes = {
+		onSuccess: PropTypes.func.isRequired,
+		twoFactorAuthType: PropTypes.string.isRequired,
+	};
+
+	render() {
+		const { twoFactorAuthType, onSuccess } = this.props;
+		return (
+			<div>
+				{
+					twoFactorAuthType === 'push'
+					? ( <WaitingTwoFactorNotificationApproval onSuccess={ onSuccess } /> )
+					: ( <VerificationCodeInput onSuccess={ onSuccess } /> )
+				}
+			</div>
+		);
+	}
+}
+
+export default connect( () => ( {
+	twoFactorAuthType: 'code',
+} ) )( localize( Login2FA ) );
---
+++ client/blocks/login/two-factor-authentication/verification-code-input.jsx
@@ -0,0 +1,140 @@
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import ExternalLink from 'components/external-link';
+import FormButtonsBar from 'components/forms/form-buttons-bar';
+import FormButton from 'components/forms/form-button';
+import FormTextInput from 'components/forms/form-text-input';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormCheckbox from 'components/forms/form-checkbox';
+import FormLabel from 'components/forms/form-label';
+import FormInputValidation from 'components/forms/form-input-validation';
+import Card from 'components/card';
+import { localize } from 'i18n-calypso';
+
+class VerificationCodeInput extends Component {
+	state = {
+		twoStepCode: '',
+		rememberMe: false,
+		error: null
+	};
+
+	onChangeField = ( event ) => {
+		if ( event.target.type === 'checkbox' ) {
+			this.setState( {
+				[ event.target.name ]: event.target.checked
+			} );
+			return;
+		}
+		// Reset the error state if the user updates the field after an error coming
+		// from the state
+		this.setState( {
+			[ event.target.name ]: event.target.value,
+			error: null
+		} );
+	};
+
+	onCodeSubmit = ( event ) => {
+		const { twoStepId, twoStepNonce } = this.props;
+		const { twoStepCode, remember } = this.state;
+		event.preventDefault();
+		this.props.loginUserWithTwoFactorVerificationCode( twoStepId, twoStepCode, twoStepNonce, remember ).then( () => {
+			this.props.onSuccess( this.state );
+		} );
+	};
+
+	componentWillMount() {
+		this.setState( {
+			error: this.props.error
+		} );
+	}
+
+	componentWillReceiveProps( newProps ) {
+		this.setState( {
+			error: newProps.error
+		} );
+	}
+
+	componentDidUpdate() {
+		if ( this.props.isLoginSuccessful ) {
+			window.location.href = ( this.props.redirectLocation || '/' );
+		}
+	}
+
+	render() {
+		const { translate } = this.props;
+		const isError = !! this.state.error;
+		let errorText = this.state.error;
+		if ( isError ) {
+			errorText = translate( 'Invalid verification code' );
+		}
+		return (
+			<div>
+				<form onSubmit={ this.onCodeSubmit }>
+					<Card>
+						<p>
+							{ translate( 'Please enter the verification code generated' +
+								' by your Authenticator mobile application.' ) }
+						</p>
+						<FormFieldset>
+							<FormLabel htmlFor="twoStepCode">
+								{ translate( 'Verification Code' ) }
+							</FormLabel>
+							<FormTextInput
+								onChange={ this.onChangeField }
+								className={ classNames( { 'is-error': isError } ) }
+								name="twoStepCode" />
+							{ isError && (
+								<FormInputValidation isError={ isError } text={ errorText } />
+							) }
+						</FormFieldset>
+						<FormFieldset>
+							<FormLabel htmlFor="rememberMe">
+								<FormCheckbox
+									name="rememberMe"
+									onChange={ this.onChangeField } />
+								<span>
+									{ translate( 'Remember for 30 days' ) }
+								</span>
+							</FormLabel>
+						</FormFieldset>
+						<FormButtonsBar>
+							<FormButton onClick={ this.onSubmit } primary>{ translate( 'Log in' ) }</FormButton>
+						</FormButtonsBar>
+					</Card>
+				</form>
+				<p>
+					<ExternalLink
+						icon={ true }
+						target="_blank"
+						href="http://en.support.wordpress.com/security/two-step-authentication/">
+						{ translate( 'Help' ) }
+					</ExternalLink>
+				</p>
+				<hr />
+				<p>
+					<a href="#">{ translate( 'Send recovery code via text' ) }</a>
+				</p>
+			</div>
+		);
+	}
+}
+
+export default connect(
+	() => ( {
+		twoStepId: 11111,
+		twoStepNonce: '5d5e1fgs55160a11ab15',
+		error: '',
+		isLoginSuccessful: null
+	} ),
+	{
+		loginUserWithTwoFactorVerificationCode: new Promise(),
+	}
+)( localize( VerificationCodeInput ) );
